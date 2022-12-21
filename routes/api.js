07e9353df5aad05817024633d4a2911e0d902bac
calliast <muharipan@gmail.com>
2022-12-21 15:12:44 +0700

    1.0 - Added full working bread features

---
+++ routes/api.js
@@ -0,0 +1,96 @@
+var express = require("express");
+var router = express.Router();
+var models = require("../models");
+var { sequelize } = require("../models/index");
+var { Response } = require("../helpers/util");
+const { Op } = require("sequelize");
+
+router.route("/test").get(function (req, res) {
+  res.send("hello");
+});
+
+router
+  .route("/phonebooks")
+  /* GET phonebook listing. */
+  .get(async function (req, res) {
+    try {
+      const { name, phone } = req.query;
+      let params = {};
+
+      if (name) {
+        params["name"] = {
+          [Op.iLike]: `%${name}%`,
+        };
+      }
+      if (phone) {
+        params["phone"] = sequelize.where(
+          sequelize.cast(sequelize.col("Phonebook.phone"), "varchar"),
+          { [Op.iLike]: `%${phone}%` }
+        );
+      }
+
+      const phonebook = await models.Phonebook.findAll({
+        where: params,
+        order: [["createdAt", "ASC"]],
+      });
+      res.send(new Response(phonebook));
+    } catch (error) {
+      res.status(500).json(new Response(error, false));
+    }
+  })
+  /* Create a phonebook. */
+  .post(async function (req, res) {
+    try {
+      const phonebook = await models.Phonebook.create(req.body);
+      res.send(new Response(phonebook));
+    } catch (error) {
+      res.status(500).json(new Response(error, false));
+    }
+  });
+
+router
+  .route("/phonebooks/:id")
+  /* Update a phonebook. */
+  .get(async function (req, res) {
+    try {
+      console.log(`kok kesini`);
+      const phonebook = await models.Phonebook.findAll({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.send(new Response(phonebook));
+    } catch (error) {
+      res.status(500).json(new Response(error, false));
+    }
+  })
+  /* Update a phonebook. */
+  .put(async function (req, res) {
+    try {
+      const phonebook = await models.Phonebook.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+        returning: true,
+        plain: true,
+      });
+      res.send(new Response(phonebook[1]));
+    } catch (error) {
+      res.status(500).json(new Response(error, false));
+    }
+  })
+  /* Delete a phonebook. */
+  .delete(async function (req, res) {
+    try {
+      const phonebook = await models.Phonebook.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.send(new Response(phonebook));
+    } catch (error) {
+      res.status(500).json(new Response(error, false));
+    }
+  });
+
+module.exports = router;
