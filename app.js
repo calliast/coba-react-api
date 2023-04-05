3676d44ca6b40ec028002f478550664242e57dda
ixhsan <muharipan@gmail.com>
2023-04-06 06:27:10 +0700

    added graphql version

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var { graphqlHTTP } = require("express-graphql");
+var { resolvers } = require("./resolvers/resolvers");
+var { schema } = require("./schema/schema.gql");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api')
+var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.use(cors())
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', apiRouter);
+app.use("/", indexRouter);
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: false,
+  })
+);
 
 module.exports = app;
